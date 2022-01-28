66a0dd59969dc2b711d5b9417216781869db2a45
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2022-01-28 02:03:22 -0500

    Spike B Repair

--- src/main/java/edu/wpi/teamname/views/MapViewController.java
+++ src/main/java/edu/wpi/teamname/views/MapViewController.java
@@ -1,6 +1,5 @@
 package edu.wpi.teamname.views;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import javafx.collections.FXCollections;
@@ -35,27 +34,28 @@ public class MapViewController {
   private void comboBoxChanged() throws FileNotFoundException {
     System.out.println(dropDown.getValue().toString());
     // need to figure out how to switch images
-//    switch (dropDown.getValue().toString()) {
-//      case "Ground Floor":
-//        FileInputStream stream =
-//            new FileInputStream("resources\\edu\\wpi\\teamname\\images\\00_thegroundfloor.png");
-//        mapImageView.setImage(new Image(stream));
-//        break;
-//      case "Lower Level 1":
-//        mapImageView.setImage(new Image("@../images/00_thelowerlevel1.png"));
-//        break;
-//      case "Lower Level 2":
-//        mapImageView.setImage(new Image("images/00_thelowerlevel2.png"));
-//        break;
-//      case "First Floor":
-//        mapImageView.setImage(new Image("@images/01_thefirstfloor.png"));
-//        break;
-//      case "Second Floor":
-//        mapImageView.setImage(new Image("@images/02_thesecondfloor.png"));
-//        break;
-//      case "Third Floor":
-//        mapImageView.setImage(new Image("@../images/03_thethirdfloor.png"));
-//        break;
-//    }
+    //    switch (dropDown.getValue().toString()) {
+    //      case "Ground Floor":
+    //        FileInputStream stream =
+    //            new
+    // FileInputStream("resources\\edu\\wpi\\teamname\\images\\00_thegroundfloor.png");
+    //        mapImageView.setImage(new Image(stream));
+    //        break;
+    //      case "Lower Level 1":
+    //        mapImageView.setImage(new Image("@../images/00_thelowerlevel1.png"));
+    //        break;
+    //      case "Lower Level 2":
+    //        mapImageView.setImage(new Image("images/00_thelowerlevel2.png"));
+    //        break;
+    //      case "First Floor":
+    //        mapImageView.setImage(new Image("@images/01_thefirstfloor.png"));
+    //        break;
+    //      case "Second Floor":
+    //        mapImageView.setImage(new Image("@images/02_thesecondfloor.png"));
+    //        break;
+    //      case "Third Floor":
+    //        mapImageView.setImage(new Image("@../images/03_thethirdfloor.png"));
+    //        break;
+    //    }
   }
 }
