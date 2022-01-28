aa3c020d829487b1fca991230adc68eed9059b77
Amitai5 <aberfanian@outlook.com>
2022-01-27 22:32:00 -0500

    Fixed Code

--- src/main/java/edu/wpi/teamname/views/MainPageViewController.java
+++ src/main/java/edu/wpi/teamname/views/MainPageViewController.java
@@ -1,8 +1,21 @@
 package edu.wpi.teamname.views;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 public class MainPageViewController {
   @FXML
-  private void setup() {}
+  private void setup() throws FileNotFoundException {
+    StackPane sp = new StackPane();
+    Image img = new Image("../images/00_thegroundfloor.png");
+    ImageView imgView = new ImageView(img);
+    sp.getChildren().add(imgView);
+  }
 }
