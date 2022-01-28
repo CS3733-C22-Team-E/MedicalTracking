f6c661137db81e0bff53295299bf368dc5deba85
Amitai5 <aberfanian@outlook.com>
2022-01-27 22:42:42 -0500

    Joe Takes Over :)

--- src/main/java/edu/wpi/teamname/views/MainPageViewController.java
+++ src/main/java/edu/wpi/teamname/views/MainPageViewController.java
@@ -1,21 +1,22 @@
 package edu.wpi.teamname.views;
 
-import java.io.FileInputStream;
+import com.jfoenix.controls.JFXComboBox;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.util.HashMap;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
 
 public class MainPageViewController {
+  @FXML private JFXComboBox mapSelectionComboBox;
+  private HashMap<String, Image> mapImages = new HashMap<String, Image>();
+
   @FXML
   private void setup() throws FileNotFoundException {
-    StackPane sp = new StackPane();
-    Image img = new Image("../images/00_thegroundfloor.png");
-    ImageView imgView = new ImageView(img);
-    sp.getChildren().add(imgView);
+    mapImages.put("Ground Floor", new Image("\\images\\00_thegroundfloor.png"));
+    mapImages.put("Level 1", new Image("\\images\\00_thelowerlevel1.png"));
+
+    for (String levelName : mapImages.keySet()) {
+      mapSelectionComboBox.getItems().add(levelName);
+    }
   }
 }
