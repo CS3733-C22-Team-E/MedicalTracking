0b67ee71d196a2a24be0b38268be2f8fbd4a6f49
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2022-01-27 23:55:55 -0500

    Nothing Good

--- src/main/java/edu/wpi/teamname/views/MapViewController.java
+++ src/main/java/edu/wpi/teamname/views/MapViewController.java
@@ -1,20 +1,19 @@
 package edu.wpi.teamname.views;
 
-import com.jfoenix.controls.JFXComboBox;
-import java.util.HashMap;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
 
 public class MapViewController {
-  private HashMap<String, Image> mapImages = new HashMap<String, Image>();
-  @FXML JFXComboBox mapSelectionComboBox;
-  @FXML ImageView mapImageView;
+  // private HashMap<String, Image> mapImages = new HashMap<String, Image>();
+  @FXML private ComboBox dropDown;
+  @FXML private ImageView mapImageView;
 
   @FXML
   private void setup() {
-    mapSelectionComboBox.setItems(
+    System.out.println("HI");
+    dropDown.setItems(
         FXCollections.observableArrayList(
             "Ground Floor",
             "Lower Level 1",
@@ -26,25 +25,25 @@ public class MapViewController {
 
   @FXML
   private void comboBoxChanged() {
-    switch (mapSelectionComboBox.getValue().toString()) {
-      case "Ground Floor":
-        mapImageView.setImage(new Image("@../images/00_thegroundfloor.png"));
-        break;
-      case "Lower Level 1":
-        mapImageView.setImage(new Image("@../images/00_thelowerlevel1.png"));
-        break;
-      case "Lower Level 2":
-        mapImageView.setImage(new Image("@../images/00_thelowerlevel2.png"));
-        break;
-      case "First Floor":
-        mapImageView.setImage(new Image("@../images/01_thefirstfloor.png"));
-        break;
-      case "Second Floor":
-        mapImageView.setImage(new Image("@../images/02_thesecondfloor.png"));
-        break;
-      case "Third Floor":
-        mapImageView.setImage(new Image("@../images/03_thethirdfloor.png"));
-        break;
-    }
+    //    switch (mapSelectionComboBox.getValue().toString()) {
+    //      case "Ground Floor":
+    //        mapImageView.setImage(new Image("@../images/00_thegroundfloor.png"));
+    //        break;
+    //      case "Lower Level 1":
+    //        mapImageView.setImage(new Image("@../images/00_thelowerlevel1.png"));
+    //        break;
+    //      case "Lower Level 2":
+    //        mapImageView.setImage(new Image("@../images/00_thelowerlevel2.png"));
+    //        break;
+    //      case "First Floor":
+    //        mapImageView.setImage(new Image("@../images/01_thefirstfloor.png"));
+    //        break;
+    //      case "Second Floor":
+    //        mapImageView.setImage(new Image("@../images/02_thesecondfloor.png"));
+    //        break;
+    //      case "Third Floor":
+    //        mapImageView.setImage(new Image("@../images/03_thethirdfloor.png"));
+    //        break;
+    //    }
   }
 }
