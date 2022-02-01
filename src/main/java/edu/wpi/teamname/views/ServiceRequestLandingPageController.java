7e58506dd85ebd5e9ac74ed84182544e3f6e1a54
Amitai5 <aberfanian@outlook.com>
2022-01-31 21:23:04 -0500

    Fixed the Tab Layout
    - All pages now need to accept the stylesheet

--- src/main/java/edu/wpi/teamname/views/ServiceRequestLandingPageController.java
+++ src/main/java/edu/wpi/teamname/views/ServiceRequestLandingPageController.java
@@ -5,74 +5,105 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextAlignment;
 
 public class ServiceRequestLandingPageController {
-  @FXML private AnchorPane internalPatientTransportationTab;
-  @FXML private AnchorPane externalPatientTransportationTab;
-  @FXML private AnchorPane computerServiceRequestTab;
-  @FXML private AnchorPane securityServiceRequestTab;
-  @FXML private AnchorPane langugageInterpreterTab;
-  @FXML private AnchorPane audioVisualServicesTab;
-  @FXML private AnchorPane sanitationServicesTab;
-  @FXML private AnchorPane religiousRequestTab;
-  @FXML private AnchorPane medicalEquipmentTab;
-  @FXML private AnchorPane medicineDeliveryTab;
-  @FXML private AnchorPane laundryServicesTab;
-  @FXML private AnchorPane foodDeliveryTab;
-  @FXML private AnchorPane giftDeliveryTab;
+  @FXML private Tab internalPatientTransportationTab;
+  @FXML private Tab externalPatientTransportationTab;
+  @FXML private Tab computerServiceRequestTab;
+  @FXML private Tab securityServiceRequestTab;
+  @FXML private Tab langugageInterpreterTab;
+  @FXML private Tab audioVisualServicesTab;
+  @FXML private Tab sanitationServicesTab;
+  @FXML private Tab religiousRequestTab;
+  @FXML private Tab medicalEquipmentTab;
+  @FXML private Tab medicineDeliveryTab;
+  @FXML private Tab laundryServicesTab;
+  @FXML private Tab foodDeliveryTab;
+  @FXML private Tab giftDeliveryTab;
+  @FXML private Tab homeTab;
+
+  @FXML private TabPane tabContainer;
+  private double tabHeight = 150;
+  private double tabWidth = 40;
 
   @FXML
   private void setup() throws IOException {
-    Parent foodDeliveryPage =
-        FXMLLoader.load(App.class.getResource("Views/FoodDeliveryServiceRequest.fxml"));
-    foodDeliveryTab.getChildren().add(foodDeliveryPage);
+    tabContainer.setTabMaxHeight(tabHeight);
+    tabContainer.setRotateGraphic(true);
+    tabContainer.setMaxWidth(tabWidth);
 
-    Parent giftDeliveryPage =
-        FXMLLoader.load(App.class.getResource("Views/GiftandFloralServiceRequest.fxml"));
-    giftDeliveryTab.getChildren().add(giftDeliveryPage);
+    configureTab(homeTab, "Home", null);
+    configureTab(medicalEquipmentTab, "", null);
+    configureTab(medicineDeliveryTab, "", null);
+    configureTab(sanitationServicesTab, "", null);
+    configureTab(externalPatientTransportationTab, "", null);
+    configureTab(internalPatientTransportationTab, "", null);
 
-    Parent laundryServicesPage =
-        FXMLLoader.load(App.class.getResource("Views/LaundryServiceRequest.fxml"));
-    laundryServicesTab.getChildren().add(laundryServicesPage);
+    configureTab(
+            foodDeliveryTab,
+            "Food Delivery",
+            "Views/FoodDeliveryServiceRequest.fxml");
 
-    Parent languageInterpreterDeliveryPage =
-        FXMLLoader.load(App.class.getResource("Views/LanguageInterpreterServiceRequest.fxml"));
-    langugageInterpreterTab.getChildren().add(languageInterpreterDeliveryPage);
+    configureTab(
+            giftDeliveryTab,
+            "Gift Delivery",
+            "Views/GiftandFloralServiceRequest.fxml");
 
-    Parent audioVideoServiceRequestPage =
-        FXMLLoader.load(App.class.getResource("Views/AudioVisualServiceRequest.fxml"));
-    audioVisualServicesTab.getChildren().add(audioVideoServiceRequestPage);
+    configureTab(
+            laundryServicesTab,
+            "Laundry Services",
+            "Views/LaundryServiceRequest.fxml");
 
-    Parent computerServiceRequestPage =
-        FXMLLoader.load(App.class.getResource("Views/ComputerServiceRequest.fxml"));
-    computerServiceRequestTab.getChildren().add(computerServiceRequestPage);
+    configureTab(
+        langugageInterpreterTab,
+        "Language Services",
+        "Views/LanguageInterpreterServiceRequest.fxml");
 
-    Parent religiousServicesPage =
-        FXMLLoader.load(App.class.getResource("Views/ReligiousServiceRequest.fxml"));
-    religiousRequestTab.getChildren().add(religiousServicesPage);
+    configureTab(
+            audioVisualServicesTab,
+            "Audio/Video Services",
+            "Views/AudioVisualServiceRequest.fxml");
 
-    Parent securityServiceRequestPage =
-        FXMLLoader.load(App.class.getResource("Views/SecurityServiceRequestController.fxml"));
-    securityServiceRequestTab.getChildren().add(securityServiceRequestPage);
+    configureTab(
+            computerServiceRequestTab,
+            "Computer Services",
+            "Views/ComputerServiceRequest.fxml");
+
+    configureTab(
+            religiousRequestTab,
+            "Religious Services",
+            "Views/ReligiousServiceRequest.fxml");
+
+    configureTab(
+        securityServiceRequestTab,
+        "Security Services",
+        "Views/SecurityServiceRequestController.fxml");
+
+    configureTab(
+        securityServiceRequestTab,
+        "Security Service Request",
+        "Views/SecurityServiceRequestController.fxml");
   }
 
-  @FXML
-  private void religiousServicesButtonClick() throws IOException {
-    Parent pane = FXMLLoader.load(App.class.getResource("Views/ReligiousServiceRequest.fxml"));
-    App.changeScene(pane);
-  }
+  private void configureTab(Tab tab, String title, String pageUrl) throws IOException {
+    Label label = new Label(title);
+    label.setTextAlignment(TextAlignment.CENTER);
 
-  @FXML
-  private void mapPageButtonClick() throws IOException {
-    Parent pane = FXMLLoader.load(App.class.getResource("Views/MapView.fxml"));
-    App.changeScene(pane);
-  }
+    AnchorPane anchorPane = new AnchorPane();
+    anchorPane.getChildren().add(label);
+    anchorPane.setRotate(90.0);
 
-  @FXML
-  private void securityServiceRequestButtonClick() throws IOException {
-    Parent pane =
-        FXMLLoader.load(App.class.getResource("Views/SecurityServiceRequestController.fxml"));
-    App.changeScene(pane);
+    tab.setGraphic(anchorPane);
+    tab.setText("");
+
+    if (pageUrl != null) {
+      Parent newPage = FXMLLoader.load(App.class.getResource(pageUrl));
+      tab.setContent(newPage);
+    }
   }
 }
