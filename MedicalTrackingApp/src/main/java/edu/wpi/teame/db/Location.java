109e6370048d5da9f676c27dada1b4be22299cb0
Amitai5 <aberfanian@outlook.com>
2022-02-01 21:44:00 -0500

    Added

---
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/db/Location.java
@@ -0,0 +1,11 @@
+package edu.wpi.teame.db;
+
+public class Location {
+    private String id;
+    private LocationType type;
+
+    private BuildingType building;
+    private FloorType floor;
+    private int x;
+    private int y;
+}
