b6c7fd7b8775ce17fdf59e4ed9e1c508cf35dbee
Amitai5 <aberfanian@outlook.com>
2022-02-02 14:28:54 -0500

    Fixed Landing Page

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/views/LandingPageController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/views/LandingPageController.java
@@ -2,17 +2,24 @@ package edu.wpi.teame.views;
 
 import edu.wpi.teame.App;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Screen;
+import lombok.SneakyThrows;
 
-public class LandingPageController {
+public class LandingPageController implements Initializable {
   @FXML private Tab internalPatientTransportationTab;
   @FXML private Tab externalPatientTransportationTab;
   @FXML private Tab computerServiceRequestTab;
@@ -34,8 +41,9 @@ public class LandingPageController {
   private double tabHeight = 250;
   private double tabWidth = 35;
 
-  @FXML
-  private void setup() throws IOException {
+  @Override
+  @SneakyThrows
+  public void initialize(URL location, ResourceBundle resources) {
     mainAnchorPane.setPrefHeight(Screen.getPrimary().getBounds().getHeight());
     mainAnchorPane.setPrefWidth(Screen.getPrimary().getBounds().getWidth());
     mainAnchorPane.autosize();
@@ -47,12 +55,8 @@ public class LandingPageController {
     configureTab(homeTab, "Home", "views/HomePage.fxml");
     configureTab(mapTab, "Hospital Map", "views/MapPage.fxml");
     configureTab(medicalEquipmentTab, "Medical Equipment", null);
+    configureTab(medicineDeliveryTab, "Medicine Delivery", null);
     configureTab(sanitationServicesTab, "Sanitation Services", null);
-
-    configureTab(
-        medicineDeliveryTab,
-        "Medicine Delivery",
-        "views/serviceRequests/MedicineDeliveryServiceRequestPage.fxml");
 
     configureTab(
         externalPatientTransportationTab,
@@ -117,15 +121,35 @@ public class LandingPageController {
 
     AnchorPane anchorPane = new AnchorPane();
     anchorPane.getChildren().add(label);
-    anchorPane.setRotate(90.0);
+    label.setRotate(90.0);
 
     tab.setGraphic(anchorPane);
     tab.setText("");
 
     if (pageUrl != null) {
-      Parent newPage = FXMLLoader.load(App.class.getResource(pageUrl));
-      newPage.setStyle("@../viewStyleSheets/mainStyle.css");
-      tab.setContent(newPage);
+      Parent pageNode = new FXMLLoader(App.class.getResource(pageUrl)).load();
+      pageNode.setStyle("@../viewStyleSheets/mainStyle.css");
+
+      // Create a gridPane to center the page we load into the tab
+      GridPane gridPane = createGridPane();
+      gridPane.add(pageNode, 0, 0);
+      tab.setContent(gridPane);
     }
+  }
+
+  private GridPane createGridPane() {
+    GridPane gridPane = new GridPane();
+    gridPane.setAlignment(Pos.CENTER);
+
+    ColumnConstraints columnConstraints = new ColumnConstraints();
+    RowConstraints rowConstraints = new RowConstraints();
+    columnConstraints.setHalignment(HPos.CENTER);
+    columnConstraints.setHgrow(Priority.ALWAYS);
+    rowConstraints.setValignment(VPos.CENTER);
+    rowConstraints.setVgrow(Priority.ALWAYS);
+
+    gridPane.getColumnConstraints().add(columnConstraints);
+    gridPane.getRowConstraints().add(rowConstraints);
+    return gridPane;
   }
 }
