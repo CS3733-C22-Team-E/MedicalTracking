112c49204c671d5fcf4c22fe00243103e88f78ba
Jose Morales <jamoralesl2021@gmail.com>
2022-02-01 22:17:43 -0500

    Creates table

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/db/DBManager.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/db/DBManager.java
@@ -35,9 +35,37 @@ public final class DBManager {
       e.printStackTrace();
     }
 
+    //creating the table for the locations
     try {
-      String createLocationsTable = ""
-    }catch ()
+      String createLocationsTable = "CREATE TABLE LOCATIONS(id VARCHAR(10) Primary Key,"
+              + "x VARCHAR(4), "
+              + "y VARCHAR(4), "
+              + "floor VARCHAR(2), "
+              + "building VARCHAR(25), "
+              + "locationType VARCHAR(4))";
+      stmt.execute(createLocationsTable);
+
+      /*
+      String createEquipmentTable = "CREATE TABLE EQUIPMENT(id VARCHAR(10) Primary Key,"
+              + "x VARCHAR(4), "
+              + "y VARCHAR(4), "
+              + "floor VARCHAR(2), "
+              + "building VARCHAR(25), "
+              + "locationType VARCHAR(4))";
+      stmt.execute(createLocationsTable);
+
+      String createEquipmentServiceRequestTable = "CREATE TABLE SERVICEREQUEST(id VARCHAR(10) Primary Key,"
+              + "x VARCHAR(4), "
+              + "y VARCHAR(4), "
+              + "floor VARCHAR(2), "
+              + "building VARCHAR(25), "
+              + "locationType VARCHAR(4))";
+      stmt.execute(createLocationsTable);
+
+      */
+    }catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   public ResultSet SQLSelect(String tableName) {
