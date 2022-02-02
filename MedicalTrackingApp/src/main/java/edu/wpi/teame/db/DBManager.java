c51b5771bd56ca508cc41a559598fad323b3d194
Jose Morales <jamoralesl2021@gmail.com>
2022-02-01 22:03:33 -0500

    Started to create db and tables

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/db/DBManager.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/db/DBManager.java
@@ -1,9 +1,11 @@
 package edu.wpi.teame.db;
 
-import java.sql.ResultSet;
+import java.sql.*;
 
 public final class DBManager {
   private static DBManager instance;
+  private Connection connection;
+  private Statement stmt;
 
   public static DBManager getInstance() {
     if (instance != null) {
@@ -12,9 +14,33 @@ public final class DBManager {
     return instance;
   }
 
-  private DBManager() {}
+  private DBManager() {
+    //add jdbc driver
+    try {
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      return;
+    }
+    //connect to the database
+    try {
+      connection = DriverManager.getConnection("jdbc:derby:memory:ESpikeB;create=true;username=admin;password=admin;");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
 
-  public ResultSet SQLSelect(String tableName, ) {
+    try {
+      stmt = connection.createStatement();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      String createLocationsTable = ""
+    }catch ()
+  }
+
+  public ResultSet SQLSelect(String tableName) {
     return null;
   }
 }
