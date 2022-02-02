102ec16432fba9f310bef6e823ce3db1b1ed89f1
brindavenk8 <bvenkataraman@wpi.edu>
2022-02-02 12:08:49 -0500

    Brinda writes code

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/db/LocationType.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/db/LocationType.java
@@ -1,16 +1,16 @@
 package edu.wpi.teame.db;
 
 public enum LocationType {
-    DEPT,
-    EXIT,
-    HALL,
-    INFO,
-    LABS,
-    REST,
-    RETL,
-    SERV,
-    STAI,
-    ELEV,
-    STOR,
-    BATH
+  DEPT,
+  EXIT,
+  HALL,
+  INFO,
+  LABS,
+  REST,
+  RETL,
+  SERV,
+  STAI,
+  ELEV,
+  STOR,
+  BATH
 }
