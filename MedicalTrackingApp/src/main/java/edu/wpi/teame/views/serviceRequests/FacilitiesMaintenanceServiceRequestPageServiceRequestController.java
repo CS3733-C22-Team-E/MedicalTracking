10a2263d72c21f67ad3dd566848859e711e39d49
HaohaoYi <siriusyi1020@gmail.com>
2022-02-02 15:21:33 -0500

    Add the Choose Issue by Requirement
    Add the choose issue through the requirement.

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/views/serviceRequests/FacilitiesMaintenanceServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/views/serviceRequests/FacilitiesMaintenanceServiceRequestPageServiceRequestController.java
@@ -1,7 +1,9 @@
 package edu.wpi.teame.views.serviceRequests;
 
+import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -14,18 +16,23 @@ public class FacilitiesMaintenanceServiceRequestPageServiceRequestController
 
   @FXML private Text facilitiesMaintenanceServiceRequestText;
   @FXML private Text nameText;
-  @FXML private Text facilityNameText;
+  @FXML private Text roomNumText;
   @FXML private Text floorText;
   @FXML private Text dateText;
   @FXML private Text issueDescriptionText;
+  @FXML private Text chooseIssueText;
 
   @FXML private TextField reporterName;
-  @FXML private TextField facilityName;
+  @FXML private TextField roomNum;
   @FXML private TextField floorNum;
   @FXML private TextField issueDescription;
 
   @FXML private DatePicker reportDate;
 
+  @FXML private JFXComboBox<String> chooseIssue;
+
   @Override
-  public void initialize(URL location, ResourceBundle resources) {}
+  public void initialize(URL location, ResourceBundle resources) {
+    chooseIssue.setItems(FXCollections.observableArrayList("Elevator", "Power", "Other"));
+  }
 }
