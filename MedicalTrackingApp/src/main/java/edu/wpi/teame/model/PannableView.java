fe0bcb8f6f82dc139bbaf17c417e43e22757fe22
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-02-03 18:24:27 -0500

    hamburgerMenu displays

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/model/PannableView.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/model/PannableView.java
@@ -19,6 +19,8 @@ public class PannableView {
 
   private boolean hamburgerOpen = false;
 
+  private ScrollPane hamburgerMenu;
+
   public PannableView(String imageURL) {
     backgroundImage =
         new Image(Pannable.class.getResource("images/map/00_thelowerlevel1.png").toString());
@@ -33,8 +35,10 @@ public class PannableView {
     // wrap the scene contents in a pannable scroll pane.
     ScrollPane scroll = createScrollPane(layout);
 
+    initHamburgerMenu();
+
     StackPane staticWrapper = new StackPane();
-    staticWrapper.getChildren().setAll(scroll, createHamburgerButton());
+    staticWrapper.getChildren().setAll(scroll, createHamburgerButton(), hamburgerMenu);
 
     // show the scene.
     Scene scene = new Scene(staticWrapper);
@@ -67,15 +71,48 @@ public class PannableView {
   }
 
   private JFXButton createHamburgerButton() {
-    Image hamburgerIcon = new Image(Pannable.class.getResource("images/Icons/HamburgerMenu.png").toString());
+    Image hamburgerIcon =
+        new Image(Pannable.class.getResource("images/Icons/HamburgerMenu.png").toString());
     ImageView icon = new ImageView(hamburgerIcon);
     icon.setFitHeight(30);
     icon.setFitWidth(30);
     final JFXButton hamburgerButton = new JFXButton("", icon);
     hamburgerButton.setTranslateX(WIDTH / 2 - (icon.getFitWidth() + 10));
     hamburgerButton.setTranslateY(-(HEIGHT / 2 - (icon.getFitHeight() + 10)));
-    hamburgerButton.setOnAction((event) -> { hamburgerOpen ^= hamburgerOpen; });
+    hamburgerButton.setOnAction(
+        (event) -> {
+          hamburgerOpen = !hamburgerOpen;
+          updateHamburgerMenu(hamburgerMenu);
+        });
     return hamburgerButton;
+  }
+
+  private void initHamburgerMenu() {
+    String[] allIcons = {"EquipmentStorageIcon.png", "HospitalBedIcon.png"};
+    hamburgerMenu = new ScrollPane();
+    hamburgerMenu.setPrefSize(100, 100);
+    populateHamburgerMenu(hamburgerMenu, allIcons);
+    hamburgerMenu.setVisible(false);
+  }
+
+  private void populateHamburgerMenu(ScrollPane scrollPane, String[] icons) {
+    GridPane grid = new GridPane();
+    grid.setVgap(20);
+    int iconNum = 0;
+    for (String icon : icons) {
+      ImageView node =
+          new ImageView(new Image(Pannable.class.getResource("images/Icons/" + icon).toString()));
+      node.setFitWidth(30);
+      node.setFitHeight(30);
+      grid.add(node, 0, iconNum);
+    }
+    grid.setTranslateX(0);
+    grid.setTranslateY(0);
+    scrollPane.setContent(grid);
+  }
+
+  private void updateHamburgerMenu(ScrollPane hamburgerMenu) {
+    hamburgerMenu.setVisible(hamburgerOpen);
   }
 
   private ScrollPane createScrollPane(Pane layout) {
