c4fc8f2b943d88112431092ce71e6d1c9f7cb4cd
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-02-06 19:57:14 -0500

    Added checkbox. Not functional.

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/model/ServiceRequestBacklog.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/model/ServiceRequestBacklog.java
@@ -5,6 +5,7 @@ import static javafx.application.Application.launch;
 import edu.wpi.teame.db.DBManager;
 import edu.wpi.teame.db.MedicalEquipmentServiceRequest;
 import edu.wpi.teame.model.enums.ServiceRequestTypes;
+import java.util.HashMap;
 import java.util.LinkedList;
 import javafx.scene.Parent;
 import javafx.scene.control.ScrollPane;
@@ -14,6 +15,7 @@ public class ServiceRequestBacklog {
 
   GridPane requestHolder = new GridPane();
   ScrollPane scrollWrapper = new ScrollPane();
+  private HashMap<Integer, GridPane> cardHashMap = new HashMap<Integer, GridPane>();
   int cardCursor = 0;
   private double SCENEWIDTH;
   private double SCENEHEIGHT;
@@ -40,20 +42,34 @@ public class ServiceRequestBacklog {
 
   public void getFromDB() {
     LinkedList<MedicalEquipmentServiceRequest> medicalEquipmentServiceRequests =
-        DBManager.getInstance().getMEServiceRequestManager().getAll();
+            DBManager.getInstance().getMEServiceRequestManager().getAll();
   }
 
   public void addServiceRequestCard(ServiceRequestCard c) {
-    requestHolder.add(c.getCard(500, 100), 0, cardCursor);
+    GridPane card = c.getCard(500, 100);
+    requestHolder.add(card, 0, cardHashMap.size());
+    cardHashMap.put(cardCursor, card);
     cardCursor++;
   }
 
   private void testAddSR(int num) {
     ServiceRequestCard testCard =
-        new ServiceRequestCard(
-            ServiceRequestTypes.MedicalEquipment, "Test Card", "Test", 0, SCENEWIDTH, SCENEHEIGHT);
+            new ServiceRequestCard(
+                    this,
+                    ServiceRequestTypes.MedicalEquipment,
+                    "Test Card",
+                    "Test",
+                    0,
+                    SCENEWIDTH,
+                    SCENEHEIGHT,
+                    cardCursor);
     for (int i = 0; i < num; i++) {
       addServiceRequestCard(testCard);
     }
+  }
+
+  // TODO Fix this method. Checkbox doens't do anything yet
+  public void removeServiceRequest(int ID) {
+    requestHolder.getChildren().removeIf(node -> GridPane.getRowIndex(node) == ID);
   }
 }
--- MedicalTrackingApp/src/main/java/edu/wpi/teame/model/ServiceRequestCard.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/model/ServiceRequestCard.java
@@ -1,6 +1,8 @@
 package edu.wpi.teame.model;
 
+import com.jfoenix.controls.JFXCheckBox;
 import edu.wpi.teame.model.enums.ServiceRequestTypes;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -16,33 +18,68 @@ public class ServiceRequestCard {
   private ServiceRequestTypes type;
   private double WIDTH;
   private double HEIGHT;
+  private ServiceRequestBacklog backlog;
+  private int backlogID;
+
+  // Styling
+  private Color BORDERCOLOR = Color.LIGHTGRAY;
 
   public ServiceRequestCard(
+      ServiceRequestBacklog backlog,
       ServiceRequestTypes SRType,
       String SRDescription,
       String SRTitle,
       int SRColor,
       double cardWidth,
-      double cardHeight) {
+      double cardHeight,
+      int ID) {
+    backlog = backlog;
     hexColor = SRColor;
     title = SRTitle;
     description = SRDescription;
     type = SRType;
     WIDTH = cardWidth;
     HEIGHT = cardHeight;
+    backlogID = ID;
   }
 
   public GridPane getCard(double width, double height) {
+    // Setup grid
     GridPane card = new GridPane();
     card.setBorder(
         new Border(
             new BorderStroke(
-                Color.DARKGREY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5))));
+                BORDERCOLOR, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(5))));
     card.setPrefSize(width, height);
+
+    card.add(getDoneCheckbox(), 0, 0);
+    card.add(getTitleText(), 1, 0);
+
+    return card;
+  }
+
+  private Text getTitleText() {
     Text titleText = new Text(title);
     titleText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
     titleText.setTextAlignment(TextAlignment.CENTER);
-    card.add(titleText, 0, 0);
-    return card;
+    return titleText;
+  }
+
+  private JFXCheckBox getDoneCheckbox() {
+    JFXCheckBox doneBox = new JFXCheckBox();
+    doneBox.setCheckedColor(Color.LIGHTBLUE);
+    doneBox.setCheckedColor(Color.LIGHTGRAY);
+    doneBox.setOnMouseClicked(
+        (event -> {
+          deleteRequest(backlog);
+        }));
+      Tooltip t = new Tooltip("Click to delete");
+      Tooltip.install(doneBox, t);
+    return doneBox;
+  }
+
+  private void deleteRequest(ServiceRequestBacklog b) {
+    b.removeServiceRequest(this.backlogID);
+    System.out.println("Deleting ID: " + this.backlogID);
   }
 }
