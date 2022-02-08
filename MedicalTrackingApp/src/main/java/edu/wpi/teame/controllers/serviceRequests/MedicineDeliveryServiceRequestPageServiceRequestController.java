f4a35ba8656daad55f1cad98dfcc04c0a44d1c87
ccordoba464 <ccordobaescobar464@gmail.com>
2022-02-08 18:51:32 -0500

    Included setItems method for the JFXComboBox that were added to the four SR.

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/controllers/serviceRequests/MedicineDeliveryServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/controllers/serviceRequests/MedicineDeliveryServiceRequestPageServiceRequestController.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+
+import edu.wpi.teame.db.DBManager;
+import edu.wpi.teame.db.Location;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -27,7 +32,19 @@ public class MedicineDeliveryServiceRequestPageServiceRequestController
   @FXML public JFXComboBox serviceAssignee;
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {}
+  public void initialize(URL location, ResourceBundle resources) {
+    // creates a linkedList of locations and sets all the values as one of roomNumber comboBox items
+    LinkedList<Location> locations = DBManager.getInstance().getLocationManager().getAll();
+    LinkedList<String> locationName = new LinkedList<String>();
+    for (Location loc : locations) {
+      locationName.add(loc.getLongName());
+    }
+
+    // Set the comboBox items
+    serviceLocation.setItems(FXCollections.observableArrayList(locationName));
+    serviceAssignee.setItems(
+            FXCollections.observableArrayList("Test Name", "Test Name", "Test Name", "Test Name"));
+  }
 
   @FXML
   private void clearText() {
--- MedicalTrackingApp/src/main/java/edu/wpi/teame/controllers/serviceRequests/SecurityServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/controllers/serviceRequests/SecurityServiceRequestPageServiceRequestController.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+
+import edu.wpi.teame.db.DBManager;
+import edu.wpi.teame.db.Location;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -24,7 +29,19 @@ public class SecurityServiceRequestPageServiceRequestController extends ServiceR
   @FXML private JFXCheckBox completed;
 
   @FXML
-  public void initialize(URL location, ResourceBundle resources) {}
+  public void initialize(URL location, ResourceBundle resources) {
+    // creates a linkedList of locations and sets all the values as one of roomNumber comboBox items
+    LinkedList<Location> locations = DBManager.getInstance().getLocationManager().getAll();
+    LinkedList<String> locationName = new LinkedList<String>();
+    for (Location loc : locations) {
+      locationName.add(loc.getLongName());
+    }
+
+    // Set the comboBox items
+    serviceLocation.setItems(FXCollections.observableArrayList(locationName));
+    serviceAssignee.setItems(
+            FXCollections.observableArrayList("Test Name", "Test Name", "Test Name", "Test Name"));
+  }
 
   @FXML
   void clearText() {
