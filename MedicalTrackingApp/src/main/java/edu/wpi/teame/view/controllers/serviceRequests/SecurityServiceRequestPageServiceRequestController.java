75faa9ec077dc290f32c87975fa73077eab3de3f
Amitai5 <aberfanian@outlook.com>
2022-02-08 22:40:26 -0500

    Security Service Request Send To DB

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/SecurityServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/SecurityServiceRequestPageServiceRequestController.java
@@ -1,7 +1,14 @@
 package edu.wpi.teame.view.controllers.serviceRequests;
 
 import com.jfoenix.controls.JFXButton;
+import edu.wpi.teame.db.DBManager;
+import edu.wpi.teame.model.Employee;
+import edu.wpi.teame.model.Location;
+import edu.wpi.teame.model.enums.ServiceRequestStatus;
+import edu.wpi.teame.model.serviceRequests.SecurityServiceRequest;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -14,6 +21,21 @@ public class SecurityServiceRequestPageServiceRequestController extends ServiceR
 
   @FXML
   public void initialize(URL location, ResourceBundle resources) {}
+
+  @FXML
+  void sendToDB() throws SQLException {
+    Employee employee = DBManager.getInstance().getEmployeeManager().getAll().get(0);
+
+    int roomID = Integer.parseInt(roomTextBox.getText());
+    Location location = DBManager.getInstance().getLocationManager().get(roomID);
+
+    SecurityServiceRequest serviceRequest =
+        new SecurityServiceRequest(
+            ServiceRequestStatus.OPEN, employee, location, new Date(new java.util.Date().getTime()), new Date(0), 0);
+    serviceRequest = DBManager.getInstance().getSecuritySRManager().insert(serviceRequest);
+
+    System.out.println();
+  }
 
   @FXML
   void clearText() {
