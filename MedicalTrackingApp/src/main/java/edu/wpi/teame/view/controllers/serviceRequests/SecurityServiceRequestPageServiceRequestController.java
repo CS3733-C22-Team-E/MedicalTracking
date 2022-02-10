6a34710a4c03d5c021646cf99cd8308fdb15825b
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2022-02-10 14:44:33 -0500

    Got rid of random ass fields in Security Service

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/SecurityServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/SecurityServiceRequestPageServiceRequestController.java
@@ -16,11 +16,8 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
 
 public class SecurityServiceRequestPageServiceRequestController extends ServiceRequestController {
-  @FXML private TextField floorTextBox;
-  @FXML private TextField roomTextBox;
   @FXML private JFXButton clearButton;
   @FXML private JFXButton sendButton;
 
@@ -80,7 +77,6 @@ public class SecurityServiceRequestPageServiceRequestController extends ServiceR
 
   @FXML
   void clearText() {
-    floorTextBox.setText("");
-    roomTextBox.setText("");
+    // TODO implement clear
   }
 }
