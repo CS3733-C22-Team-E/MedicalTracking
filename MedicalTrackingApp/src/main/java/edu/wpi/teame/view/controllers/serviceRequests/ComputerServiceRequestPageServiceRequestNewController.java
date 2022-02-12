0fff0168a2935f98e8ca317444285fc8271aed4f
Jose Morales <jamoralesl2021@gmail.com>
2022-02-12 16:15:50 -0500

    Added the fields and started to play around with text area

---
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/ComputerServiceRequestPageServiceRequestNewController.java
@@ -0,0 +1,24 @@
+package edu.wpi.teame.view.controllers.serviceRequests;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ComputerServiceRequestPageServiceRequestNewController extends ServiceRequestController {
+
+  @FXML
+  ComboBox locationComboBox;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+  //todo write clearText()
+}
