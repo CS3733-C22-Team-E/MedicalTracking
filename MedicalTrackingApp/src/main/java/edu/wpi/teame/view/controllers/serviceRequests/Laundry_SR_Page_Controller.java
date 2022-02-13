220a0b9bd94328549be47df0115f5fc43b44b1b7
miyengar26 <miyengar@wpi.edu>
2022-02-13 18:33:29 -0500

    maanav service request

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/Laundry_SR_Page_Controller.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/Laundry_SR_Page_Controller.java
@@ -1,15 +1,15 @@
 package edu.wpi.teame.view.controllers.serviceRequests;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Laundry_SR_Page_Controller extends ServiceRequestController {
-  @FXML public JFXButton send;
-  @FXML public JFXButton clear;
+  @FXML public Button send;
+  @FXML public Button clear;
   @FXML private TextField requestDate;
   @FXML private TextField location;
   @FXML private TextField assignee;
