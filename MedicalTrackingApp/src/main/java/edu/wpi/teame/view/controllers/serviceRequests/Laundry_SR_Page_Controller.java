a0730713f299081ef66cec4ef56709ff51f2cc7a
miyengar26 <miyengar@wpi.edu>
2022-02-13 19:04:06 -0500

    Completely Finished Service Request

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/Laundry_SR_Page_Controller.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/Laundry_SR_Page_Controller.java
@@ -5,26 +5,31 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Laundry_SR_Page_Controller extends ServiceRequestController {
   @FXML public Button send;
   @FXML public Button clear;
-  @FXML private TextField requestDate;
+  @FXML private DatePicker requestDate;
   @FXML private TextField location;
   @FXML private TextField assignee;
   @FXML private JFXComboBox<String> priority;
   @FXML private JFXComboBox<String> status;
+  @FXML private TextArea additionalInfo;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
 
   @FXML
   private void clearText() {
-    requestDate.setText("");
+    requestDate.setValue(null);
+    requestDate.getEditor().clear();
     location.setText("");
     assignee.setText("");
     priority.valueProperty().set(null);
     status.valueProperty().set(null);
+    additionalInfo.setText("");
   }
 }
