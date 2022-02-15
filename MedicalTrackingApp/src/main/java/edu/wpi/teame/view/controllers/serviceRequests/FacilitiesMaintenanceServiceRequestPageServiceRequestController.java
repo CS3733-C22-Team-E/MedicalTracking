9063390e8aa58dc1e1325f56ed4b81a3bee7ddd2
brindavenk8 <bvenkataraman@wpi.edu>
2022-02-14 21:15:53 -0500

    facilities sr ui remake - not functional

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/FacilitiesMaintenanceServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/FacilitiesMaintenanceServiceRequestPageServiceRequestController.java
@@ -1,49 +1,93 @@
 package edu.wpi.teame.view.controllers.serviceRequests;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import edu.wpi.teame.db.DBManager;
+import edu.wpi.teame.model.Employee;
+import edu.wpi.teame.model.Location;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import edu.wpi.teame.model.enums.ServiceRequestStatus;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.scene.control.TextArea;
+import lombok.SneakyThrows;
 
 public class FacilitiesMaintenanceServiceRequestPageServiceRequestController
     extends ServiceRequestController {
-  @FXML public Button sendButton;
+  @FXML private DatePicker requestDatePicker;
+  @FXML private JFXComboBox locationComboBox;
+  @FXML private JFXComboBox employeeComboBox;
+  @FXML private JFXComboBox priorityComboBox;
+  @FXML private JFXComboBox statusComboBox;
+  @FXML private TextArea descriptionTextArea;
 
-  @FXML private Text facilitiesMaintenanceServiceRequestText;
-  @FXML private Text nameText;
-  @FXML private Text roomNumText;
-  @FXML private Text floorText;
-  @FXML private Text dateText;
-  @FXML private Text issueDescriptionText;
-  @FXML private Text chooseIssueText;
+  @FXML private JFXButton clearButton;
+  @FXML public JFXButton sendButton;
+  private boolean hasRun = false;
 
-  @FXML private TextField reporterName;
-  @FXML private TextField roomNum;
-  @FXML private TextField floorNum;
-  @FXML private TextField issueDescription;
-
-  @FXML private DatePicker reportDate;
-
-  @FXML private JFXComboBox<String> chooseIssue;
-
-  @Override
+  @FXML
+  @SneakyThrows
   public void initialize(URL location, ResourceBundle resources) {
-    chooseIssue.setItems(FXCollections.observableArrayList("Elevator", "Power", "Other"));
+    //todo: implement initialize
+    locationComboBox.setItems(FXCollections.observableArrayList());
+    employeeComboBox.setItems(FXCollections.observableArrayList());
+    priorityComboBox.setItems(FXCollections.observableArrayList());
+    statusComboBox.setItems(FXCollections.observableArrayList(ServiceRequestStatus.values()));
+
+
   }
 
   @FXML
   private void clearText() {
-    reporterName.setText("");
-    floorNum.setText("");
-    roomNum.setText("");
-    issueDescription.setText("");
-    reportDate.setValue(null);
-    reportDate.getEditor().clear();
-    chooseIssue.valueProperty().set(null);
+    requestDatePicker.valueProperty().set(null);
+    locationComboBox.valueProperty().set(null);
+    employeeComboBox.valueProperty().set(null);
+    priorityComboBox.valueProperty().set(null);
+    statusComboBox.valueProperty().set(null);
+    descriptionTextArea.setText("");
+  }
+
+  @FXML
+  public void sendToDB() throws SQLException { //todo: make functional
+    // get values from ComboBoxes
+    Date date;
+    String destination = (String) locationComboBox.getValue();
+    int employeeID = (int) employeeComboBox.getValue();
+    int priority = (int) priorityComboBox.getValue();
+
+    // make a new facilities SR, select * from Facilities SR table, insert
+
+  }
+
+  @FXML
+  public void updateFromDB() throws SQLException {
+    if (hasRun) {
+      return;
+    }
+    hasRun = true;
+
+    // creates a linkedList of locations and sets all the values as one of roomNumber comboBox items
+    List<Location> locations = DBManager.getInstance().getLocationManager().getAll();
+    List<Employee> employees = DBManager.getInstance().getEmployeeManager().getAll();
+
+    List<String> locationNames = new LinkedList<String>();
+    for (Location loc : locations) {
+      locationNames.add(loc.getLongName());
+    }
+
+    List<String> employeeNames = new LinkedList<String>();
+    for (Employee emp : employees) {
+      employeeNames.add(emp.getName());
+    }
+
+    locationComboBox.setItems(FXCollections.observableArrayList(locationNames));
+    employeeComboBox.setItems(FXCollections.observableArrayList(employeeNames));
   }
 }
