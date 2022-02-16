c1c55b3b343c24fa7532d81e97213943c057e999
Jose Morales <jamoralesl2021@gmail.com>
2022-02-15 20:02:11 -0500

    continued removed methods calls from startup

--- MedicalTrackingApp/src/test/java/edu/wpi/teame/TestObjectManager.java
+++ MedicalTrackingApp/src/test/java/edu/wpi/teame/TestObjectManager.java
@@ -79,29 +79,6 @@ public class TestObjectManager {
                     "as");
 
     LocationManager location = new LocationManager();
-    location.insert(loc1);
-    location.insert(loc2);
-    location.insert(loc3);
-    location.insert(loc4);
-
-    LinkedList<Location> result = (LinkedList<Location>) location.getAll();
-    for (Location loc : result) {
-      System.out.println(loc);
-    }
-
-    location.update(
-            new Location(
-                    1,
-                    "Center for something",
-                    1617,
-                    825,
-                    FloorType.FirstFloor,
-                    BuildingType.Tower,
-                    LocationType.DEPT,
-                    "some"));
-
-    System.out.println(location.get(1));
-    System.out.println();
 
     Equipment equipment1 =
             new Equipment(1, loc1, EquipmentType.PBED, "Hospital Bed 1", true, false);
@@ -111,21 +88,7 @@ public class TestObjectManager {
             new Equipment(3, loc3, EquipmentType.PUMP, "Infusion Pump 1", true, true);
 
     EquipmentManager equipmentManager = new EquipmentManager();
-    equipmentManager.insert(equipment1);
-    equipmentManager.insert(equipment2);
-    equipmentManager.insert(equipment3);
 
-    List<Equipment> equipmentLinkedList = equipmentManager.getAll();
-    for (Equipment equip : equipmentLinkedList) {
-      System.out.println(equip);
-    }
-
-    equipmentManager.update(
-            new Equipment(1, loc1, EquipmentType.PBED, "Hospital Bed for jose", true, true));
-    System.out.println(equipmentManager.get(1));
-    System.out.println();
-
-    /** int id, DepartmentType dept, String name, EmployeeType type */
     Employee emp1 = new Employee(1, DepartmentType.FAMILYMEDICINE, "Shannon L", EmployeeType.Admin);
     Employee emp2 =
             new Employee(2, DepartmentType.CLINICALSERVICES, "Madelyn Pearson", EmployeeType.Staff);
@@ -134,47 +97,17 @@ public class TestObjectManager {
                     3, DepartmentType.PLASTICSURGERY, "John Ronald Reagan Tolkien", EmployeeType.Admin);
 
     EmployeeManager employeeManager = new EmployeeManager();
-    employeeManager.insert(emp1);
-    employeeManager.insert(emp2);
-    employeeManager.insert(emp3);
 
-    List<Employee> employeeList = employeeManager.getAll();
-    for (Employee emp : employeeList) {
-      System.out.println(emp);
-    }
-
-    System.out.println("updating employee...");
-    employeeManager.update(
-            new Employee(1, DepartmentType.PLASTICSURGERY, "Jose L", EmployeeType.Admin));
-    employeeList = employeeManager.getAll();
-    for (Employee emp : employeeList) {
-      System.out.println(emp);
-    }
-
-    System.out.println();
 
     long d = System.currentTimeMillis();
     Date date = new Date(d);
-    DateFormat dateFormat = new SimpleDateFormat("MMddyyyy");
 
     Patient patient1 = new Patient(loc1, date, "Jose Morales", 1);
     Patient patient2 = new Patient(loc2, date, "Josie Morales", 2);
     Patient patient3 = new Patient(loc3, date, "Joseph Morales", 3);
 
     PatientManager pman = new PatientManager();
-    pman.insert(patient1);
-    pman.insert(patient2);
-    pman.insert(patient3);
 
-    System.out.println("updating patient...");
-    pman.update(new Patient(loc1, date, "Jose Morales 2.0", 1));
-
-    System.out.println();
-    /**
-     * DataBaseObjectType type, ServiceRequestPriority priority, ServiceRequestStatus status, String
-     * additionalInfo, Employee assignee, Location location, Date requestDate, Date closeDate, Date
-     * openDate, String title, int id
-     */
     ServiceRequest secReq1 =
             new ServiceRequest(
                     DataBaseObjectType.SecuritySR,
@@ -216,37 +149,6 @@ public class TestObjectManager {
                     3);
 
     StandardSRManager secReqManager = new StandardSRManager(DataBaseObjectType.SecuritySR);
-    secReqManager.insert(secReq1);
-    secReqManager.insert(secReq2);
-    secReqManager.insert(secReq3);
-
-    System.out.println("updating standardSR...");
-    secReqManager.update(
-            new ServiceRequest(
-                    DataBaseObjectType.SecuritySR,
-                    ServiceRequestPriority.Normal,
-                    ServiceRequestStatus.CLOSED,
-                    "I need swecurity heheheheheheh",
-                    emp3,
-                    loc3,
-                    Date.valueOf("2017-03-31"),
-                    null,
-                    date,
-                    "titel",
-                    3));
-    System.out.println();
-
-    List<ServiceRequest> securitylist = secReqManager.getAll();
-    for (ServiceRequest sec : securitylist) {
-      System.out.println(sec);
-    }
-
-    //     secReqManager.update(
-    //             new ServiceRequest(DataBaseObjectType.SecuritySR, ServiceRequestPriority.High,
-    // ServiceRequestStatus.OPEN, "I need swecurity", emp1, loc1,  Date.valueOf("2015-03-31"), null,
-    // date, "titel",0));
-    //     System.out.println(secReqManager.get(1));
-    //     System.out.println();
 
     ServiceRequest sanReq1 =
             new ServiceRequest(
@@ -289,36 +191,7 @@ public class TestObjectManager {
                     3);
 
     StandardSRManager sanReqManager = new StandardSRManager(DataBaseObjectType.SanitationSR);
-    sanReqManager.insert(sanReq1);
-    sanReqManager.insert(sanReq2);
-    sanReqManager.insert(sanReq3);
 
-    System.out.println("updating sanitationSR...");
-    sanReqManager.update(
-            new ServiceRequest(
-                    DataBaseObjectType.SanitationSR,
-                    ServiceRequestPriority.Critical,
-                    ServiceRequestStatus.PENDING,
-                    "I need sanitatrion",
-                    emp3,
-                    loc3,
-                    Date.valueOf("2015-05-31"),
-                    date,
-                    date,
-                    "titel",
-                    3));
-
-    List<ServiceRequest> sanReqlist = sanReqManager.getAll();
-    for (ServiceRequest san : sanReqlist) {
-      System.out.println(san);
-    }
-    System.out.println();
-
-    //     sanReqManager.update(
-    //         new SanitationServiceRequest(ServiceRequestStatus.CLOSED, emp1, loc1, date, date,
-    // 1));
-    //
-    //     System.out.println();
 
     MedicineDeliveryServiceRequest medReq1 =
             new MedicineDeliveryServiceRequest(
@@ -367,44 +240,7 @@ public class TestObjectManager {
                     patient3);
 
     MedicineDeliverySRManager medReqManager = new MedicineDeliverySRManager();
-    medReqManager.insert(medReq1);
-    medReqManager.insert(medReq2);
-    medReqManager.insert(medReq3);
 
-    System.out.println("updating medicineDeliverySR...");
-    medReqManager.update(
-            new MedicineDeliveryServiceRequest(
-                    ServiceRequestPriority.Normal,
-                    ServiceRequestStatus.PENDING,
-                    "I need sanitatrion",
-                    emp3,
-                    loc3,
-                    Date.valueOf("2015-03-31"),
-                    null,
-                    date,
-                    "titel",
-                    3,
-                    "djkfdshgfuiowelkfjbvhdjkfs",
-                    "od",
-                    patient3));
-
-    List<MedicineDeliveryServiceRequest> medReqlist = medReqManager.getAll();
-    for (MedicineDeliveryServiceRequest san : medReqlist) {
-      System.out.println(san);
-    }
-    System.out.println();
-    //     medReqManager.update(
-    //         new MedicineDeliveryServiceRequest(
-    //             ServiceRequestStatus.CLOSED, emp1, loc1, date, date, 1, date));
-
-    //     System.out.println(medReqManager.get(1));
-    //     System.out.println();
-
-    /**
-     * ServiceRequestPriority priority, ServiceRequestStatus status, String additionalInfo, Employee
-     * assignee, Location location, Date requestDate, Date closeDate, Date openDate, String title,
-     * int id, Equipment equipment
-     */
     MedicalEquipmentSRManager medEqpManager = new MedicalEquipmentSRManager();
     MedicalEquipmentServiceRequest sr1 =
             new MedicalEquipmentServiceRequest(
@@ -448,10 +284,6 @@ public class TestObjectManager {
                     3,
                     equipment3);
 
-    medEqpManager.insert(sr1);
-    medEqpManager.insert(sr2);
-    medEqpManager.insert(sr3);
-    
     FoodDeliveryServiceRequest food1 =
             new FoodDeliveryServiceRequest(
                     ServiceRequestPriority.Normal,
