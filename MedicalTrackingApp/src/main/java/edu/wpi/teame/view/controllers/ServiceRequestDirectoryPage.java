54b21abf266ab0c19f43af6281cd68fc053ab49e
ccordoba464 <ccordobaescobar464@gmail.com>
2022-02-15 23:39:59 -0500

    Included header2 in mainStyle.css and fields in directory controller

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/ServiceRequestDirectoryPage.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/ServiceRequestDirectoryPage.java
@@ -2,10 +2,35 @@ package edu.wpi.teame.view.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import edu.wpi.teame.App;
+import edu.wpi.teame.model.enums.DataBaseObjectType;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import static edu.wpi.teame.model.enums.DataBaseObjectType.LaundrySR;
 
 public class ServiceRequestDirectoryPage implements Initializable {
 
+  @FXML private Button securitySR;
+  @FXML private Button audioVisualSR;
+  @FXML private Button externalSR;
+  @FXML private Button facilitiesSR;
+  @FXML private Button computerSR;
+  @FXML private Button giftFloralSR;
+  @FXML private Button internalSR;
+  @FXML private Button foodSR;
+  @FXML private Button languageSR;
+  @FXML private Button laundrySR;
+  @FXML private Button medEquipSR;
+  @FXML private Button medDeliverySR;
+  @FXML private Button religiousSR;
+  @FXML private Button sanitationSR;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
+
+
 }
