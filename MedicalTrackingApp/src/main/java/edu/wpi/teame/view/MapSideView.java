c8c6b83b87f4e3fa664c852f6ca17e81ffe4d70f
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2022-02-16 13:01:08 -0500

    MapSideView actually works correctly

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/MapSideView.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/MapSideView.java
@@ -8,11 +8,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.event.EventHandler;
-import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -27,7 +28,10 @@ public class MapSideView {
   private double MAPHEIGHT = 1343;
   private double MAPWIDTH = 859;
   private int[][] EquipmentManagementArray = new int[2][4];
-  ListView<Equipment> newListview = new ListView<Equipment>();
+  ListView<Equipment> AllEquipmentView = new ListView<Equipment>();
+  GridPane holder = new GridPane();
+  public ArrayList<TextField> cleanFields = new ArrayList<TextField>();
+  public ArrayList<TextField> dirtyFields = new ArrayList<TextField>();
 
   public MapSideView() {
     for (int i = 0; i < 4; ++i) {
@@ -35,11 +39,37 @@ public class MapSideView {
         this.EquipmentManagementArray[i][j] = 0;
       }
     }
+    for (int i = 0; i < 4; i++) {
+      TextField clean = new TextField("0");
+      clean.setMaxWidth(35);
+      cleanFields.add(clean);
+      TextField dirty = new TextField("0");
+      dirty.setMaxWidth(35);
+      dirtyFields.add(dirty);
+      holder.add(cleanFields.get(i), i + 1, 1);
+      holder.add(dirtyFields.get(i), i + 1, 2);
+    }
     backgroundImage =
         new Image(App.class.getResource("images/map/Tower - Side View.jpg").toString());
-    newListview.setPrefSize(600, 200);
-    newListview.setMaxSize(newListview.getPrefWidth(), newListview.getPrefHeight());
-    newListview.getStyleClass().add("combo-box");
+    AllEquipmentView.setPrefSize(600, 200);
+    AllEquipmentView.setMaxSize(AllEquipmentView.getPrefWidth(), AllEquipmentView.getPrefHeight());
+    AllEquipmentView.getStyleClass().add("combo-box");
+    holder.setAlignment(Pos.CENTER);
+    Label Clean = new Label("Clean");
+    Label Dirty = new Label("Dirty");
+    holder.add(Clean, 0, 1);
+    holder.add(Dirty, 0, 2);
+    Label PBED = new Label("Patient Beds");
+    Label XRAY = new Label("XRAYs");
+    Label INFUSIONPUMP = new Label("Infusion Pumps");
+    Label RECLINER = new Label("Recliners");
+    holder.add(PBED, 1, 0);
+    holder.add(INFUSIONPUMP, 2, 0);
+    holder.add(XRAY, 3, 0);
+    holder.add(RECLINER, 4, 0);
+    holder.setMaxSize(600, 200);
+    holder.setHgap(10);
+    holder.setVgap(10);
   }
 
   public Parent getMapScene() {
@@ -48,15 +78,10 @@ public class MapSideView {
     StackPane staticWrapper = new StackPane();
     GridPane Grid = new GridPane();
     Grid.setMouseTransparent(true);
-    Grid.getChildren().add(newListview);
-    Grid.setVgrow(newListview, Priority.ALWAYS);
+    Grid.getChildren().add(AllEquipmentView);
+    Grid.setVgrow(AllEquipmentView, Priority.ALWAYS);
     Grid.setAlignment(Pos.TOP_CENTER);
     staticWrapper.getChildren().add(scroll);
-    layout.setOnMouseClicked(
-        event -> {
-          System.out.println(
-              layout.sceneToLocal(new Point2D(event.getSceneX(), event.getSceneY())));
-        });
     Rectangle Floor3 = this.createRectangle(125.0D, 1025.0D, 413.0D, 52.0D);
     Rectangle Floor1 = this.createRectangle(125.0D, 1125.0D, 413.0D, 52.0D);
     Rectangle LL1 = this.createRectangle(121.0D, 1175.0D, 415.0D, 52.0D);
@@ -64,13 +89,17 @@ public class MapSideView {
     LL1.setOnMouseEntered(
         new EventHandler<MouseEvent>() {
           public void handle(MouseEvent event) {
+            for (int i = 0; i < 4; i++) {
+              EquipmentManagementArray[0][i] = 0;
+              EquipmentManagementArray[1][i] = 0;
+            }
             try {
-              newListview.getItems().clear();
+              AllEquipmentView.getItems().clear();
               MapSideView.this.getEquipmentByFloorFromDB(FloorType.LowerLevel1).stream()
                   .forEach(
                       (equipment) -> {
                         System.out.println("Sorting");
-                        newListview.getItems().add(equipment);
+                        AllEquipmentView.getItems().add(equipment);
                         if (equipment.isClean()) {
                           MapSideView.this
                               .EquipmentManagementArray[0][equipment.getType().ordinal()]++;
@@ -81,19 +110,27 @@ public class MapSideView {
                       });
             } catch (SQLException var3) {
               var3.printStackTrace();
+            }
+            for (int i = 0; i < 4; i++) {
+              cleanFields.get(i).setText(String.valueOf(EquipmentManagementArray[1][i]));
+              dirtyFields.get(i).setText(String.valueOf(EquipmentManagementArray[0][i]));
             }
           }
         });
     Floor1.setOnMouseEntered(
         new EventHandler<MouseEvent>() {
           public void handle(MouseEvent event) {
-            newListview.getItems().clear();
+            for (int i = 0; i < 4; i++) {
+              EquipmentManagementArray[0][i] = 0;
+              EquipmentManagementArray[1][i] = 0;
+            }
+            AllEquipmentView.getItems().clear();
             try {
               MapSideView.this.getEquipmentByFloorFromDB(FloorType.FirstFloor).stream()
                   .forEach(
                       (equipment) -> {
                         System.out.println("Sorting");
-                        newListview.getItems().add(equipment);
+                        AllEquipmentView.getItems().add(equipment);
                         if (equipment.isClean()) {
                           MapSideView.this
                               .EquipmentManagementArray[0][equipment.getType().ordinal()]++;
@@ -104,19 +141,27 @@ public class MapSideView {
                       });
             } catch (SQLException var3) {
               var3.printStackTrace();
+            }
+            for (int i = 0; i < 4; i++) {
+              cleanFields.get(i).setText(String.valueOf(EquipmentManagementArray[1][i]));
+              dirtyFields.get(i).setText(String.valueOf(EquipmentManagementArray[0][i]));
             }
           }
         });
     Floor3.setOnMouseEntered(
         new EventHandler<MouseEvent>() {
           public void handle(MouseEvent event) {
-            newListview.getItems().clear();
+            AllEquipmentView.getItems().clear();
+            for (int i = 0; i < 4; i++) {
+              EquipmentManagementArray[0][i] = 0;
+              EquipmentManagementArray[1][i] = 0;
+            }
             try {
               MapSideView.this.getEquipmentByFloorFromDB(FloorType.ThirdFloor).stream()
                   .forEach(
                       (equipment) -> {
                         System.out.println("Sorting");
-                        newListview.getItems().add(equipment);
+                        AllEquipmentView.getItems().add(equipment);
                         if (equipment.isClean()) {
                           MapSideView.this
                               .EquipmentManagementArray[0][equipment.getType().ordinal()]++;
@@ -128,12 +173,18 @@ public class MapSideView {
             } catch (SQLException var3) {
               var3.printStackTrace();
             }
+            for (int i = 0; i < 4; i++) {
+              cleanFields.get(i).setText(String.valueOf(EquipmentManagementArray[1][i]));
+              dirtyFields.get(i).setText(String.valueOf(EquipmentManagementArray[0][i]));
+            }
           }
         });
 
     layout.getChildren().addAll(Floor3, Floor1, LL1);
-    staticWrapper.getChildren().add(Grid);
-    Grid.setColumnSpan(newListview, 3);
+    holder.setMouseTransparent(true);
+    holder.getStyleClass().add("combo-box");
+    staticWrapper.getChildren().add(holder);
+    Grid.setColumnSpan(AllEquipmentView, 3);
     return staticWrapper;
   }
 
