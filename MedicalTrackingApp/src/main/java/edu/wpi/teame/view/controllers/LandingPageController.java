4f98ce8c728bba5711a7b876d3dc0c6028fff2fb
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-02-22 20:09:57 -0500

    Added AboutUs page icon

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/LandingPageController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/LandingPageController.java
@@ -165,7 +165,7 @@ public class LandingPageController implements Initializable {
             "About Page",
             SortOrder.ByName,
             "view/tabs/AboutPage.fxml",
-            new Image(App.class.getResource("images/Icons/pageIcons/SRDirectory.png").toString()));
+            new Image(App.class.getResource("images/Icons/AboutUs.png").toString()));
     TabHoverAnimation.install(aboutPageTab);
     tabs.add(aboutPageTab);
 
