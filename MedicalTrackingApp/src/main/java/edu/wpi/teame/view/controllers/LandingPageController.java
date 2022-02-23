b74db3b041b36e19ef05dd628b35b0939e3c9292
brindavenk8 <bvenkataraman@wpi.edu>
2022-02-22 23:04:34 -0500

    redid about page & made button

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/LandingPageController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/LandingPageController.java
@@ -151,15 +151,6 @@ public class LandingPageController implements Initializable {
     TabHoverAnimation.install(settingsTab);
     tabs.add(settingsTab);
 
-    StyledTab aboutPage =
-        new StyledTab(
-            "About Page",
-            SortOrder.ByName,
-            "view/tabs/AboutPage.fxml",
-            new Image(App.class.getResource("images/Icons/pageIcons/SRDirectory.png").toString()));
-    TabHoverAnimation.install(aboutPage);
-    tabs.add(aboutPage);
-
     tabs.sort(StyledTab::compareTo);
     mainTabPane.getTabs().setAll(tabs);
 
