ce8e5b00406ed1a6e9d4a6841a724791323d79d5
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-02-27 15:51:35 -0500

    Colorscheme.java

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/MentalHealthServiceRequestPageServiceRequestController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/serviceRequests/MentalHealthServiceRequestPageServiceRequestController.java
@@ -10,7 +10,7 @@ import edu.wpi.teame.model.enums.DataBaseObjectType;
 import edu.wpi.teame.model.enums.ServiceRequestPriority;
 import edu.wpi.teame.model.enums.ServiceRequestStatus;
 import edu.wpi.teame.model.serviceRequests.ServiceRequest;
-import edu.wpi.teame.view.animations.SRSentAnimation;
+import edu.wpi.teame.view.style.SRSentAnimation;
 import edu.wpi.teame.view.controllers.AutoCompleteTextField;
 import java.net.URL;
 import java.sql.Date;
