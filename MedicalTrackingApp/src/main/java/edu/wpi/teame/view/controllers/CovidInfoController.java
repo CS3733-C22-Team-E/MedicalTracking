36221e9b6828922c38b616626c43c0ac94628f94
jtrembleywpi <81537355+jtrembleywpi@users.noreply.github.com>
2022-02-28 01:13:49 -0500

    Added controller class

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/CovidInfoController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/CovidInfoController.java
@@ -1,19 +1,39 @@
 package edu.wpi.teame.view.controllers;
 
-import edu.wpi.teame.App;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
-
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class CovidInfoController implements Initializable {
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {}
+  @FXML private Button infoButton;
 
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+    infoButton.setOnMouseClicked(
+        listener -> {
+          try {
+            openCovidWebsite();
+          } catch (URISyntaxException e) {
+            e.printStackTrace();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        });
+  }
+
+  // TODO
+  // When infoButton is pressed it opens up the CDC covid site
+  // https://www.cdc.gov/coronavirus/2019-ncov/index.html
+  public void openCovidWebsite() throws URISyntaxException, IOException {
+    Desktop d = Desktop.getDesktop();
+    d.browse(new URI("https://www.cdc.gov/coronavirus/2019-ncov/index.html"));
+  }
 }
