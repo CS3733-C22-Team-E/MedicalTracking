f763c6f54dc8b59445433a89362e8009371a6b18
jtrembleywpi <81537355+jtrembleywpi@users.noreply.github.com>
2022-02-28 00:53:56 -0500

    Added FXML page

---
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/CovidInfoController.java
@@ -0,0 +1,19 @@
+package edu.wpi.teame.view.controllers;
+
+import edu.wpi.teame.App;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CovidInfoController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
+}
