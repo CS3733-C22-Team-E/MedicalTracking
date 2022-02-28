bd71690d4ec5f93a476cb3709fe2a6f031e2569f
ccordoba464 <ccordobaescobar464@gmail.com>
2022-02-27 21:37:55 -0500

    Updated CredManPaage
    Included more buttons, text fields, and a popup for the add button

--- MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/CredentialManagementPageController.java
+++ MedicalTrackingApp/src/main/java/edu/wpi/teame/view/controllers/CredentialManagementPageController.java
@@ -2,13 +2,20 @@ package edu.wpi.teame.view.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import edu.wpi.teame.App;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class CredentialManagementPageController implements Initializable {
   private List<Object> itemsList = null;
@@ -26,5 +33,29 @@ public class CredentialManagementPageController implements Initializable {
   public void removeCredentialButtonClick() throws SQLException {}
 
   @FXML
-  public void addCredentialButtonClick() throws SQLException {}
+  void handleButtonAction() {
+    try {
+      Scene scene =
+          new Scene(
+              FXMLLoader.load(App.class.getResource("view/tabs/AddCredentialPopupPage.fxml")));
+      Stage stage = new Stage();
+      stage.setTitle("Test");
+      stage.setScene(scene);
+      stage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  public void addCredentialButtonClick() throws IOException {
+    Scene scene =
+        new Scene(FXMLLoader.load(App.class.getResource("view/tabs/AddCredentialPopupPage.fxml")));
+    Stage stage = new Stage();
+    stage.setTitle("Test");
+    stage.setScene(scene);
+    stage.show();
+    stage.initModality(Modality.WINDOW_MODAL);
+    stage.initStyle(StageStyle.UNDECORATED);
+  }
 }
